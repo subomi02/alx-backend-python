a93fcc1562415392d75abe602eb717e7c9bdd5c4
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-05 23:24:48 +0100

    Update 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Task 2's module.
+'''Adetunji Olasubomi
 '''
 import asyncio
 import time
@@ -10,9 +10,6 @@ async_comprehension = using('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    '''Executes async_comprehension 4 times and measures the
-    total execution time.
-    '''
     start_time = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     return time.time() - start_time
