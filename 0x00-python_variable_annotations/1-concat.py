a3f39644de910a4be3d905fd02af702fde0d4161
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-05 22:23:45 +0100

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-'''Task 1's module.
+'''Adetunji Subomi
 '''
 
 
 def concat(str1: str, str2: str) -> str:
-    '''Concatenates two strings.
+    '''Concatenates two strings together.
     '''
     return str1 + str2
