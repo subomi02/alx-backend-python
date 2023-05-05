8e9c98759d76bb5f227f5b84a7d34d36c9487dd7
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-05 22:27:17 +0100

    Update 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-'''Task 6's module.
+'''Adetunji Olasubomi
 '''
 from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    '''Computes the sum of a list of integers and floating-point numbers.
+    '''sums a list of integers and floats.
     '''
     return float(sum(mxd_lst))
