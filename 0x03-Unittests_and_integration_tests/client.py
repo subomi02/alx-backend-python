b2244c2766e21c5027f45e2c5aa31a3129b3ecab
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-05 23:32:00 +0100

    Update client.py

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""A github org client
+"""Adetunji Olasubomi
 """
 from typing import (
     List,
@@ -14,31 +14,36 @@ from utils import (
 
 
 class GithubOrgClient:
-    """A Github org client
+    """
     """
     ORG_URL = "https://api.github.com/orgs/{org}"
 
     def __init__(self, org_name: str) -> None:
-        """Init method of GithubOrgClient"""
+        """
+        """
         self._org_name = org_name
 
     @memoize
     def org(self) -> Dict:
-        """Memoize org"""
+        """
+        """
         return get_json(self.ORG_URL.format(org=self._org_name))
 
     @property
     def _public_repos_url(self) -> str:
-        """Public repos URL"""
+        """
+        """
         return self.org["repos_url"]
 
     @memoize
     def repos_payload(self) -> Dict:
-        """Memoize repos payload"""
+        """
+        """
         return get_json(self._public_repos_url)
 
     def public_repos(self, license: str = None) -> List[str]:
-        """Public repos"""
+        """
+        """
         json_payload = self.repos_payload
         public_repos = [
             repo["name"] for repo in json_payload
@@ -49,7 +54,8 @@ class GithubOrgClient:
 
     @staticmethod
     def has_license(repo: Dict[str, Dict], license_key: str) -> bool:
-        """Static: has_license"""
+        """
+        """
         assert license_key is not None, "license_key cannot be None"
         try:
             has_license = access_nested_map(
