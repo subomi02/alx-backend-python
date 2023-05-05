b8a7706a0b14875b24b69c21ae5b3e2e2e1af7f6
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-05 23:29:52 +0100

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""A module for testing the utils module.
+"""Adetunji Olasubomi
 """
 import unittest
 from typing import Dict, Tuple, Union
@@ -14,7 +14,8 @@ from utils import (
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests the `access_nested_map` function."""
+    """
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -26,7 +27,8 @@ class TestAccessNestedMap(unittest.TestCase):
             path: Tuple[str],
             expected: Union[Dict, int],
             ) -> None:
-        """Tests `access_nested_map`'s output."""
+        """
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -39,13 +41,15 @@ class TestAccessNestedMap(unittest.TestCase):
             path: Tuple[str],
             exception: Exception,
             ) -> None:
-        """Tests `access_nested_map`'s exception raising."""
+        """
+        """
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
 
 
 class TestGetJson(unittest.TestCase):
-    """Tests the `get_json` function."""
+    """
+    """
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
@@ -55,7 +59,8 @@ class TestGetJson(unittest.TestCase):
             test_url: str,
             test_payload: Dict,
             ) -> None:
-        """Tests `get_json`'s output."""
+        """
+        """
         attrs = {'json.return_value': test_payload}
         with patch("requests.get", return_value=Mock(**attrs)) as req_get:
             self.assertEqual(get_json(test_url), test_payload)
@@ -63,9 +68,11 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    """Tests the `memoize` function."""
+    """
+    """
     def test_memoize(self) -> None:
-        """Tests `memoize`'s output."""
+        """
+        """
         class TestClass:
             def a_method(self):
                 return 42
