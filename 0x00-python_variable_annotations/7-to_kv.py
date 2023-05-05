419bbfc8e1929fb919808ffc3721f04db2d96980
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-05 22:27:45 +0100

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-'''Task 7's module.
+'''Adetunji Olasubomi
 '''
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    '''Converts a key and its value to a tuple of the key and
-    the square of its value.
+    '''a
     '''
     return (k, float(v**2))
