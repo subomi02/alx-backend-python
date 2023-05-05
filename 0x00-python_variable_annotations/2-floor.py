00b328878299a90679775680c581dcdbd9cb807a
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-05 22:24:26 +0100

    Update 2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-'''Task 2's module.
+'''Adetunji Olasubomi
 '''
 
 
 def floor(a: float) -> int:
-    '''Computes the floor of a floating-point number.
+    '''floors a floating-point number.
     '''
     return int(a)
