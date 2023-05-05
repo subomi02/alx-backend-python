b087711dc1807e14b953592481f69e8989dd2212
Olasubomi <72149243+subomi02@users.noreply.github.com>
2023-05-05 23:24:02 +0100

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Task 1's module.
+'''Adetunji Olasubomi
 '''
 from typing import List
 from importlib import import_module as using
@@ -9,6 +9,6 @@ async_generator = using('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    '''Creates a list of 10 numbers from a 10-number generator.
+    '''
     '''
     return [num async for num in async_generator()]
